a41ab5dde0b5b576a96cade44d11ed03dd303324
蛋总 <xinglong.wangwxl@alibaba-inc.com>
2016-04-17 22:32:27 +0800

    init

---
+++ script/main.js
@@ -0,0 +1,19 @@
+chrome.tabs.getSelected(null,function(tab) {
+    const url = encodeURIComponent(tab.url);
+	const title = encodeURIComponent(tab.title);
+	
+	fetch(`http://localhost:7001/send?url=${url}&title=${title}`).then(function(res) {
+		if (res.ok) {
+		    res.json().then(function(data) {
+				document.getElementById('sm-month-tip').innerText = data.text;	
+				setTimeout(function () {
+					window.close();
+				}, 2000);
+		    });
+		} else {
+			document.getElementById('sm-month-tip').innerText = '请求失败';	
+		}
+	}, function(e) {
+		document.getElementById('sm-month-tip').innerText = '请求失败';	
+	});
+});
